0f809f9cfd445c635944f522b60bed30b02773ff
EP\dais <daniel.isaacs@optimizely.com>
2024-06-18 17:09:25 -0600

    website config

---
+++ website.cjs
@@ -0,0 +1,28 @@
+// Auto generated file - do not change manually!
+
+const siteInfo = {
+    "name": "foundation-mvc-cms",
+    "id": "67a48c59-9a6c-4871-b926-267c31d4fb93",
+    "startPage": "99de17e6-0e5c-488f-93fa-15e7e4d9f660",
+    "startPageUrl": "https://optispa.opti-demo.net/en/",
+    "locales": [
+        "en"
+    ],
+    "defaultLocale": "en",
+    "labels": [
+        {
+            "code": "en",
+            "label": "English"
+        }
+    ],
+    "primaryDomain": "https://optispa.opti-demo.net",
+    "domains": [
+        "https://optispa.opti-demo.net",
+        "https://localhost:3091",
+        "https://*",
+        "https://localhost:3000"
+    ],
+    "localeDomains": []
+};
+
+module.exports = siteInfo;
