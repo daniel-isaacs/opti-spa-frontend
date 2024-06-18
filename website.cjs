f79ca40179ce400ce3e007905df7f3fd528549f6
EP\dais <daniel.isaacs@optimizely.com>
2024-06-18 17:34:07 -0600

    update website config

--- website.cjs
+++ website.cjs
@@ -4,7 +4,7 @@ const siteInfo = {
     "name": "foundation-mvc-cms",
     "id": "67a48c59-9a6c-4871-b926-267c31d4fb93",
     "startPage": "99de17e6-0e5c-488f-93fa-15e7e4d9f660",
-    "startPageUrl": "https://optispa.opti-demo.net/en/",
+    "startPageUrl": "https://opti-spa-frontend.vercel.app/en/",
     "locales": [
         "en"
     ],
@@ -15,9 +15,8 @@ const siteInfo = {
             "label": "English"
         }
     ],
-    "primaryDomain": "https://optispa.opti-demo.net",
+    "primaryDomain": "https://opti-spa-frontend.vercel.app",
     "domains": [
-        "https://optispa.opti-demo.net",
         "https://localhost:3091",
         "https://*",
         "https://localhost:3000",
